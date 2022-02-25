7103043fb31bdaf0788d58e757a6f3649c2a0b3c
JordanMurray2 <Jordan.Murray1@marist.edu>
2022-02-25 10:45:29 -0500

    started to integrate NextJS template

--- client/metrics-dashboard/pages/_app.js
+++ client/metrics-dashboard/pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
+import "assets/scss/nextjs-material-dashboard-pro.scss?v=1.0.0";
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
