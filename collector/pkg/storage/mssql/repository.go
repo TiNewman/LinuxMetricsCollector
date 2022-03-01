7d15d5183ac1f726d0d2d5b0e3abca839b296b1a
TiNewman <titan.newman1@marist.edu>
2022-02-28 19:33:12 -0500

    Fixing repository.go

--- collector/pkg/storage/mssql/repository.go
+++ collector/pkg/storage/mssql/repository.go
@@ -2,6 +2,7 @@
 For now I have main left as a comment, as it allows for easy testing.
 Will need to change function names etc, but for now we can see that a connection
 to the SQL Server works.
+To start the connection, call 'databaseConnection'.
 */
 //package mssql
 
@@ -12,6 +13,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
@@ -29,9 +31,32 @@ type Storage struct {
 	DB_CONNECTION *sql.DB
 }
 
-func main() {
+type Process struct {
+	PID           int
+	collectorID   int
+	name          string
+	status        string
+	cpuUsage      float32
+	memoryUsage   float32
+	diskUsage     float32
+	executionTime time.Time
+}
 
-	fmt.Printf("Repository Implementation for mssql (Microsoft SQL Server)\n")
+type Collector struct {
+	collectorID   int
+	timeCollected time.Time
+	cpuID         int
+	memoryID      int
+	diskID        int
+}
+
+type Cpu struct {
+	cpuID        int
+	usage        float32
+	availability float32
+}
+
+func openDBConnection() {
 
 	// Build connection string
 	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
@@ -55,13 +80,24 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	// Log connection here!
 	fmt.Printf("Connected to DB!\n")
+}
+
+func closeDBConnection() {
+
+	DB_CONNECTION.Close()
+}
+
+func getAllCPU() []Cpu {
+
+	ctx := context.Background()
 
 	// For not we are just getting from the CPU table!
-	tsql := fmt.Sprintf("SELECT * FROM CPU;")
+	singleQuery := fmt.Sprintf("SELECT * FROM CPU;")
 
 	// Execute query
-	rows, err := DB_CONNECTION.QueryContext(ctx, tsql)
+	rows, err := DB_CONNECTION.QueryContext(ctx, singleQuery)
 
 	if err != nil {
 
@@ -70,12 +106,12 @@ func main() {
 
 	defer rows.Close()
 
-	var count int
+	var toReturn []Cpu
 
 	// Iterate through the result set.
 	for rows.Next() {
 
-		var usage, availability string
+		var usage, availability float32
 		var cpuID int
 
 		// Get values from row.
@@ -86,13 +122,32 @@ func main() {
 			log.Fatal(err.Error())
 		}
 
-		fmt.Printf("cpuID: %d, usage: %s, availability: %s\n", cpuID, usage, availability)
-		count++
+		singleInput := Cpu{cpuID, usage, availability}
+		toReturn = append(toReturn, singleInput)
 	}
+
+	return toReturn
+}
+
+func main() {
+
+	fmt.Printf("Repository Implementation for mssql (Microsoft SQL Server)\n")
+
+	// To start the connection, call 'databaseConnection'.
+	openDBConnection()
+
+	/*
+		var answer []Cpu = getAllCPU()
+
+		for _, cpu := range answer {
+
+			fmt.Printf("cpuID: %d, usage: %f, availability: %f\n", cpu.cpuID, cpu.usage, cpu.availability)
+		}
+	*/
 
 	// For now I am closing it manually.
 	// Not sure if we want it to stay open......
-	DB_CONNECTION.Close()
+	closeDBConnection()
 
 	fmt.Print("Look, im done!")
 }
