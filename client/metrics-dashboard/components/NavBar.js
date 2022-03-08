cfb9cc43dd88edb29bb5bd16319ec9393dbb3bd1
JordanMurray2 <Jordan.Murray1@marist.edu>
2022-03-08 12:01:56 -0500

    fixed links between pages in navbar

--- client/metrics-dashboard/components/NavBar.js
+++ client/metrics-dashboard/components/NavBar.js
@@ -5,9 +5,9 @@ const NavBar = () => (
     <div className="navbar bg-base-100">
       <a className="btn btn-ghost normal-case text-xl">Linux Metrics Collector</a>
     </div>
-    <ul class="menu bg-secondary w-56 p-2 rounded-box">
-      <li><Link href="../pages/index"><a>Dashboard</a></Link></li>
-      <li><Link href="../pages/process_list"><a>Process List</a></Link></li>
+    <ul className="menu bg-secondary w-56 p-2 rounded-box">
+      <li><Link href="/">Dashboard</Link></li>
+      <li><Link href="/process_list">Process List</Link></li>
     </ul>
   </div>
 );
