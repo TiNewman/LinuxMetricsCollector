1b5a96d4ae4b3a13ee3716036f251696a4206e39
JordanMurray2 <Jordan.Murray1@marist.edu>
2022-03-15 14:20:05 -0400

    started websocket boiilerplate for nextJS

--- client/metrics-dashboard/components/NavBar.js
+++ client/metrics-dashboard/components/NavBar.js
@@ -1,19 +1,19 @@
 import Link from 'next/link'
 
 const NavBar = () => (
-<div class="navbar bg-base-200 rounded-box">
-  <div class="flex-1 px-2 lg:flex-none">
-    <div class="flex items-stretch">
-          <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost rounded-btn">Linux Metrics Collector</label>
-            <ul tabindex="0" class="menu dropdown-content p-2 shadow bg-secondary rounded-box w-52 mt-4">
+<div className="navbar bg-base-200 rounded-box">
+  <div className="flex-1 px-2 lg:flex-none">
+    <div className="flex items-stretch">
+          <div className="dropdown">
+            <label tabIndex="0" className="btn btn-ghost rounded-btn">Linux Metrics Collector</label>
+            <ul tabIndex="0" className="menu dropdown-content p-2 shadow bg-secondary rounded-box w-52 mt-4">
               <li><Link href="/">Dashboard</Link></li>
               <li><Link href="/process_list">Process List</Link></li>
             </ul>
           </div>
         </div>
   </div>
-  <div class="flex justify-end flex-1 px-2">
+  <div className="flex justify-end flex-1 px-2">
   </div>
 </div>
 );
--- client/metrics-dashboard/pages/process_list.js
+++ client/metrics-dashboard/pages/process_list.js
@@ -1,10 +1,14 @@
+import Link from 'next/link'
+import { useCallback, useEffect, useState } from 'react'
+import io from 'Socket.IO-client'
 import processListStyles from '../styles/Process_List.module.css'
 
-const processListView = props => (
-  <div>
+let socket
+let html = (
+ <div>
    <h1 className={processListStyles.h1}> Process List </h1>
    <div className="overflow-x-auto flex flex-col justify-center items-center">
-     <table class="table">
+     <table className="table">
        <thead>
          <tr>
            <th>PID</th>
@@ -13,53 +17,87 @@ const processListView = props => (
            <th>RAM</th>
            <th>Disk</th>
            <th>Status</th>
+           <th>Execution Time</th>
          </tr>
        </thead>
-       <tbody>
-         <tr className="hover">
-           <th>1</th>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-         </tr>
-         <tr className="hover">
-           <th>2</th>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-         </tr>
-         <tr className="hover">
-           <th>3</th>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-         </tr>
-         <tr className="hover">
-           <th>4</th>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-         </tr>
-         <tr className="hover">
-           <th>5</th>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-           <td></td>
-         </tr>
+       <tbody id="pTableBody">
        </tbody>
      </table>
    </div>
-  </div>
-);
+ </div>
+)
+
+const processListView = props => {
+  //const [message, setMessage] = useState('')
+  //use this to store the process list stuff
+  //const [process_list, setProcessList] = useState('')
+
+  useEffect(() => socketInitializer(), [])
+
+  const socketInitializer = async () => {
+    await fetch('/api/socket');
+    socket = io() //point socket to his here
+    /*
+    socket.on('connect', () => {
+      setMessage('Connected')
+    })
+
+    socket.onmessage = (e) => {
+      setMessage("Get message from server: " + e.data)
+    };
+    */
+    socket.on('connect', () => {
+      //as soon as connection happens, send request for process list
+      socket.send(JSON.stringify({
+            "request": "process_list"
+      }))
+    })
+
+    socket.onmessage = (e) => {
+      var processArray = JSON.parse(e)// might need to be e.data
+      for (var i = 0; i < processArray.process_list.length; i++) {
+          var process = processArray.process_list.counters[i];
+          console.log(process.Name);
+          createTableRow(process) //pass each process in and create a row for it
+      }
+    };
+  }
+
+  const createTableRow = (process) => {
+    //declare the new row elements
+    const tableRow = document.createElement("tr")
+    const pid = document.createElement("td")
+    const name = document.createElement("td")
+    const cpu = document.createElement("td")
+    const ram = document.createElement("td")
+    const disk = document.createElement("td")
+    const status = document.createElement("td")
+    const uptime = document.createElement("td")
+
+    //set the values of the process in the correct column
+    pid.innerHTML = process.PID
+    name.innerHTML = process.Name
+    cpu.innerHTML = process.CPU
+    ram.innerHTML = process.RAM
+    disk.innerHTML = process.DISK
+    status.innerHTML = process.STATUS
+    uptime.innerHTML = process.UpTime
+
+    //append the columns to the row
+    tableRow.appendChild(pid)
+    tableRow.appendChild(name)
+    tableRow.appendChild(cpu)
+    tableRow.appendChild(ram)
+    tableRow.appendChild(disk)
+    tableRow.appendChild(status)
+    tableRow.appendChild(uptime)
+
+    //append the row to the html table body
+    const tableBody = html.getElementbyId("pTableBody")
+    tableBody.appendChild(tableRow)
+  }
+
+  return html
+}
 
 export default processListView;
