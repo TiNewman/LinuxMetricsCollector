7f9c6dfc9b38db957be67999ac60bf49b1eb38b1
James <james.jenkins2@marist.edu>
2022-03-20 20:02:57 -0400

    Add Docker Build
    Add command to build the database docker container to the build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# if running the script from another directory, navigates to the project root dir
+cd "$(dirname -- "${BASH_SOURCE[0]}")"
+
+# build the collector component with go build
 cd collector && go build -o bin/lmc ./cmd
+
+# build the UI component with npm
 cd ../client/metrics-dashboard && npm run build
+
+# build the db docker container
+cd ../../ && sudo docker-compose build
