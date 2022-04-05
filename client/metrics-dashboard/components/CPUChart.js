e92de71cd1254819b72d9ae6125e6e9f73b25573
JordanMurray2 <Jordan.Murray1@marist.edu>
2022-04-05 16:02:00 -0400

    set up cpu chart for websocket

--- client/metrics-dashboard/components/CPUChart.js
+++ client/metrics-dashboard/components/CPUChart.js
@@ -1,29 +1,13 @@
 import React, { useState } from 'react';
 import Chart from 'react-apexcharts';
 
-const CPUChart = () => {
-  const [options, setOptions] = useState({
-    chart: {
-      id: 'line-chart',
-    },
-    xaxis: {
-      categories: [
-        '8:23:00',
-        '8:23:30',
-        '8:24:00',
-        '8:24:30',
-        '8:25:00',
-        '8:25:30',
-        '8:26:00',
-      ],
-    },
-  });
-  const [series, setSeries] = useState([
-    {
-      name: 'CPU Percentage Used',
-      data: [7.7905493, 4.123711, 5.1546392, 5.050505, 12.244898, 4.1666665, 4.0816326],
-    },
-  ]);
+const CPUChart = (options, series) => {
+  const [options, setOptions] = useState({});
+  const [series, setSeries] = useState([]);
+
+  setOptions = {options}
+  setSeries = {series}
+
   return (
     <div className='chart w-3/6 pt-7 mx-auto'>
       <Chart options={options} series={series} type='line'/>
