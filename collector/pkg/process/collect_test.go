bc639d30ddcd440eddcfb530d91d3ccdff48f9a6
James <james.jenkins2@marist.edu>
2022-04-10 15:06:31 -0400

    Begin Process Package Tests
    Begin creating test suite for the process package.

---
+++ collector/pkg/process/collect_test.go
@@ -0,0 +1,27 @@
+package process
+
+import (
+	"testing"
+)
+
+func TestCalcCPUUtilization(t *testing.T) {
+	ts := []struct {
+		name          string
+		cpuTime       float64
+		executionTime float64
+		expected      float64
+	}{
+		{name: "full utilization", cpuTime: 450.6, executionTime: 450.6, expected: float64(100)},
+		{name: "execution zero", cpuTime: 450.6, executionTime: 0, expected: float64(0)},
+		{name: "cpu zero", cpuTime: 0, executionTime: 2043.56, expected: float64(0)},
+	}
+	for _, tc := range ts {
+		// run each case in a sub-test
+		t.Run(tc.name, func(t *testing.T) {
+			result := calcCPUUtilization(float64(tc.cpuTime), float64(tc.executionTime))
+			if result != tc.expected {
+				t.Errorf("test %v; Expected: %v, Got: %v\n", tc.name, tc.expected, result)
+			}
+		})
+	}
+}
