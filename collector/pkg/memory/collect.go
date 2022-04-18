eb0f50d1d8890ae2e604ab7c77bec6b2de6d0944
James <james.jenkins2@marist.edu>
2022-04-18 13:02:47 -0400

    Implement Memory Collect Method
    Implement the Collect method in the memory package to collect the current system memory usage.

--- collector/pkg/memory/collect.go
+++ collector/pkg/memory/collect.go
@@ -1,9 +1,13 @@
 package memory
 
-import "os"
+import (
+	"os"
+
+	"github.com/prometheus/procfs"
+)
 
 type Memory struct {
-	Usage float32
+	Usage float64
 }
 
 type collector struct {
@@ -26,5 +30,33 @@ func newTestCollector(mp string) collector {
 
 func (c collector) Collect() (Memory, error) {
 	result := Memory{}
+
+	fs, err := procfs.NewFS(c.mount)
+	if err != nil {
+		// fmt.Printf("Cannot locate proc mount %v", err.Error())
+		return result, err
+	}
+
+	info, err := fs.Meminfo()
+	if err != nil {
+		// fmt.Printf("Could not get CPU info: %v\n", err)
+		return result, err
+	}
+
+	total := info.MemTotal
+	available := info.MemAvailable
+
+	usage := calculateUsage(total, available)
+
+	result = Memory{Usage: usage}
+
 	return result, nil
+}
+
+func calculateUsage(total *uint64, available *uint64) float64 {
+	ftotal := float64(*total)
+	favailable := float64(*available)
+	used := ftotal - favailable
+	usage := used / ftotal
+	return usage
 }
