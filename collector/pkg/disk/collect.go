10b91c9df63962209ead057eaf52a717bd30dce2
James <james.jenkins2@marist.edu>
2022-04-20 15:58:51 -0400

    Compute Disk Usage And Size
    Compute the disk usage and size of each disk found.

--- collector/pkg/disk/collect.go
+++ collector/pkg/disk/collect.go
@@ -1,19 +1,21 @@
 package disk
 
 import (
-	"fmt"
 	"regexp"
 
 	"github.com/prometheus/procfs"
+	"golang.org/x/sys/unix"
 )
 
 type Disk struct {
-	Usage float64
-	Size  float64
+	Name       string
+	MountPoint string
+	Usage      float64
+	Size       float64
 }
 
 type collector struct {
-	command string
+	location string // regular expresion matching the directory to search for disk devices
 }
 
 type Collector interface {
@@ -21,20 +23,11 @@ type Collector interface {
 }
 
 func NewDiskCollector() collector {
-	return collector{}
+	return collector{location: `/dev.*`}
 }
 
 func (c collector) Collect() ([]Disk, error) {
 	result := []Disk{}
-
-	/*
-		out, err := exec.Command(c.command).Output()
-		if err != nil {
-			return result, err
-		}
-
-		fmt.Printf(string(out))
-	*/
 
 	mountInfo, err := procfs.GetMounts()
 	if err != nil {
@@ -42,10 +35,24 @@ func (c collector) Collect() ([]Disk, error) {
 	}
 
 	for _, m := range mountInfo {
-		if found, _ := regexp.MatchString(`/dev.*`, m.Source); found {
-			fmt.Printf("mount info: %+v\n\n", m)
+		if found, _ := regexp.MatchString(c.location, m.Source); found {
+			// calculate disk usage and size
+			// then append to the list of disks
+			var stat unix.Statfs_t
+			// fmt.Printf("%v:\n", m.MountPoint)
+			unix.Statfs(m.MountPoint, &stat)
+			usage := calculateUsage(float64(stat.Bfree), float64(stat.Blocks))
+			// fmt.Printf("Used: %v%%\n", usage)
+			size := float64(stat.Blocks*uint64(stat.Bsize)) / 1000000
+			// fmt.Printf("Size: %v\n", size)
+
+			result = append(result, Disk{Name: m.Source, MountPoint: m.MountPoint, Usage: usage, Size: size})
 		}
 	}
 
 	return result, nil
+}
+
+func calculateUsage(free float64, total float64) float64 {
+	return (1 - (free / total)) * 100
 }
