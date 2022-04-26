9b1e9a464f72770bc67ed03c9de5436ab2240562
JordanMurray2 <Jordan.Murray1@marist.edu>
2022-04-25 21:50:45 -0400

    fixed styling of disk page and disk chart on main dashboard

--- client/metrics-dashboard/pages/disk.js
+++ client/metrics-dashboard/pages/disk.js
@@ -7,7 +7,7 @@ let socket
 
 const diskView = props => {
   //use this to store the process list stuff
-  const [diskUsage, setDiskUsage] = useState([])
+ /* const [diskUsage, setDiskUsage] = useState([])
 
   useEffect(() => socketInitializer(), [])
 
@@ -31,11 +31,12 @@ const diskView = props => {
       socket.send(JSON.stringify({"request": "stop"}))
       socket.close()
     };
-  }
+  } */
 
-  /** THIS IS THE MANUAL TEST DATA FOR CLIENT WEBSOCKETS 
-  const diskData = [{Usage:67.7905493}]
-  */
+  /** THIS IS THE MANUAL TEST DATA FOR CLIENT WEBSOCKETS */
+  //const diskData = [{Usage:67.7905493}]
+  const diskUsage = [{"Name":"/dev/nvme0n1p3","MountPoint":"/","Usage":2.060362882143396,"Size":510405.902336},{"Name":"/dev/nvme0n1p3","MountPoint":"/home","Usage":2.060362882143396,"Size":510405.902336},{"Name":"/dev/nvme0n1p2","MountPoint":"/boot","Usage":25.681494412006668,"Size":1020.70272},{"Name":"/dev/nvme0n1p1","MountPoint":"/boot/efi","Usage":2.3118672372403726,"Size":627.900416}]
+  /**/
 
   //<div className="radial-progress text-primary self-center" style={{"--value":diskUsage[0].Usage, "--size":"20rem"}}>{diskUsage[0].Usage}%</div>
       /*
@@ -48,12 +49,15 @@ const diskView = props => {
   return (
     <div>
       <h1 className={processListStyles.h1}> Disk Usage </h1>
-      {diskUsage.map((item, index) => <div className="overflow-x-center flex flex-col w-1/4 mx-auto">
-        <div className="block overflow-x-center flex flex-cols mt-5 p-5 shadow-lg shadow-primary">
-          <div className="radial-progress text-primary self-center" style={{"--value":item.Usage, "--size":"20rem"}}>{item.Usage}%</div>
-        </div>
-      </div>)}
-    </div>
+        {diskUsage.map((item, index) => <div className="overflow-x-center flex flex-col w-1/6 mx-auto ml-24 mt-5 float-left px-4">
+          <h2 className={processListStyles.h2}>{item.Name}</h2>
+          <div className="block overflow-x-center flex flex-cols mt-5 p-5 shadow-lg shadow-primary">
+            <div className="radial-progress text-primary self-center" style={{"--value":item.Usage.toFixed(2), "--size":"10rem"}}>{item.Usage.toFixed(2)}%</div>
+          </div>
+          <p className="pl-8 pt-8">Mount Point: {item.MountPoint}</p>
+          <p className="pl-8">Size: {item.Size.toFixed(2)} MB</p>
+        </div>)}
+      </div>
   )
 }
 
--- client/metrics-dashboard/pages/index.js
+++ client/metrics-dashboard/pages/index.js
@@ -60,18 +60,18 @@ const Index = () => {
      ]
 
    const cpuData = [{Usage:37.7905493}]
-   const diskData = [{Usage:67.7905493}]
+   const diskData = [{"Name":"/dev/nvme0n1p3","MountPoint":"/","Usage":2.060362882143396,"Size":510405.902336},{"Name":"/dev/nvme0n1p3","MountPoint":"/home","Usage":2.060362882143396,"Size":510405.902336},{"Name":"/dev/nvme0n1p2","MountPoint":"/boot","Usage":25.681494412006668,"Size":1020.70272},{"Name":"/dev/nvme0n1p1","MountPoint":"/boot/efi","Usage":2.3118672372403726,"Size":627.900416}]
    const ramData = [{Usage:13.7905493}]
   /**/
 
   return (
      <div>
-       <div className="float-left mt-10 pt-10 pl-56">
+       <div className="float-left mt-10 pt-10 pl-36">
          <h1 className={styles.h1}> Process List </h1>
          <Table data={process_list} column={column}/>
        </div>
        <Link href="/cpu">
-         <div className="float-left mt-10 pt-10 pl-6">
+         <div className="float-left mt-10 pt-10 pl-10">
            <h1 className={styles.h1}> CPU Usage </h1>
            <div className="block p-5 shadow-lg shadow-primary hover:bg-primary">
              <div className="radial-progress text-primary hover:text-base-100" style={{"--value":cpuData[0].Usage, "--size":"12rem"}}>{cpuData[0].Usage}%</div>
@@ -79,15 +79,15 @@ const Index = () => {
          </div>
        </Link>
        <Link href="/disk">
-         <div className="float-left mt-10 pt-10 pl-10">
-           <h1 className={styles.h1}> Disk Usage </h1>
+         <div className="float-left mt-10 pt-10 pl-16">
+           <h2 className={styles.h2}>Root Disk: {diskData[0].Name}</h2>
            <div className="block p-5 shadow-lg shadow-primary hover:bg-primary">
              <div className="radial-progress text-primary hover:text-base-100" style={{"--value":diskData[0].Usage, "--size":"12rem"}}>{diskData[0].Usage}%</div>
            </div>
          </div>
        </Link>
        <Link href="/memory">
-         <div className="float-left mt-10 pt-10 pl-10">
+         <div className="float-left mt-10 pt-10 pl-16">
            <h1 className={styles.h1}> RAM Usage </h1>
            <div className="block p-5 shadow-lg shadow-primary hover:bg-primary">
              <div className="radial-progress text-primary hover:text-base-100" style={{"--value":ramData[0].Usage, "--size":"12rem"}}>{ramData[0].Usage}%</div>
