2ba6f516868414168d89817a0e2656aea579c23c
James <james.jenkins2@marist.edu>
2022-04-27 18:00:52 -0400

    Add Persistent Storage
    Add persistent storage again to the core application using the new functional option.

--- collector/cmd/main.go
+++ collector/cmd/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/TiNewman/LinuxMetricsCollector/pkg/collecting"
@@ -12,35 +13,33 @@ import (
 	"github.com/TiNewman/LinuxMetricsCollector/pkg/logger"
 	"github.com/TiNewman/LinuxMetricsCollector/pkg/memory"
 	"github.com/TiNewman/LinuxMetricsCollector/pkg/process"
+	"github.com/TiNewman/LinuxMetricsCollector/pkg/storage/mssql"
 )
 
 func main() {
 	// initialize logging
 	logger.Init()
 
-	// fmt.Printf("Linux Metrics Collector\n")
 	logger.Info("Linux Metrics Collector Started")
 
 	// initialize storage
-	/*
-		s, err := mssql.NewStorage()
-		if err != nil {
-			fmt.Printf("Could not initialize persistent storage: %v", err.Error())
-		}
-	*/
+	s, err := mssql.NewStorage()
+	if err != nil {
+		logger.Error(fmt.Sprintf("Could not initialize persistent storage: %v", err.Error()))
+	}
 
 	// initialize collectors
 	pcollector := process.NewProcessCollectorWithoutRepo()
 	cpuCollector := cpu.NewCPUCollectorWithoutRepo()
 	memCollector := memory.NewMemoryCollector()
 	diskCollector := disk.NewDiskCollector()
+
 	collectingService := collecting.NewService(
 		collecting.WithProcessCollector(pcollector),
 		collecting.WithCPUCollector(cpuCollector),
 		collecting.WithMemCollector(memCollector),
-		collecting.WithDiskCollector(diskCollector))
-	// collectingService := collecting.NewServiceWithoutRepo(pcollector, cpuCollector)
-	// collectingService := collecting.NewService(pcollector, cpuCollector, s)
+		collecting.WithDiskCollector(diskCollector),
+		collecting.WithRepository(s))
 
 	// serve endpoints
 	router := websocket.Handler(collectingService)
